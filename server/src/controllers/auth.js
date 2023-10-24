d7c537aab90c627cc98f4ed00eb9fca8e4155b0d
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-10-25 00:17:56 +0200

    nodemailer set

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,6 +1,8 @@
 import asyncHandler from "express-async-handler";
 import { validateUserRegisterInput } from "../util/input/inputValidator.js";
 import User from "../models/User.js";
+import sendEmail from "../util/mailer/sendEmail.js";
+import { registirationEmail } from "../util/mailer/mailTemplates.js";
 
 export const register = asyncHandler(async (req, res, next) => {
   const { firstname, lastname, phone, password, email } =
@@ -17,6 +19,12 @@ export const register = asyncHandler(async (req, res, next) => {
   const userObject = user.toObject();
   delete userObject.password;
 
+  await sendEmail({
+    from: process.env.SMTP_USER,
+    to: user.email,
+    subject: "Welcome to Hotel",
+    html: registirationEmail,
+  });
   return res
     .status(201)
     .cookie("customer_access_token", token, {
---
+++ server/src/util/mailer/mailTemplates.js
@@ -0,0 +1,6 @@
+export const registirationEmail = (firstname) => {
+  return String.raw`
+  Welcome ${firstname},
+  You have registered succesfully!
+  `;
+};
