62f901ebc0a829ddcfcfeb7c4b6a17d35f92876a
Hadeel Murshed <131537843+Hadeel-Mur@users.noreply.github.com>
2023-10-25 17:57:04 +0200

    adding the room info card and the component of the room details card

---
+++ client/src/components/RoomInfoCard.jsx
@@ -0,0 +1,50 @@
+import  {useState} from 'react'
+import { Link } from 'react-router-dom';
+import "./CSS/room.infoCard.css"
+import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from "react-icons/bs"
+
+function RoomInfoCard({ data }) { 
+    const [slide, setSlide] = useState(0)
+
+    const nextSlide = () => {
+     setSlide(slide === data.length - 1 ? 0 : slide +1 )
+    }
+
+    const prevSlide = () => {
+     setSlide(slide === 0 ? data.length -1 : slide - 1)
+    }
+
+  return (
+    <div className='block'>
+     <div className='carousel'>
+        <BsArrowLeftCircleFill className='arrow arrow-left' onClick={prevSlide}/>
+        {data.map((item, idx) => {
+            return <img src={item.src} alt={item.alt} key={idx} className={slide === idx ? 'slide' : 'slide slide-hidden'}/>
+        })}
+        <BsArrowRightCircleFill className='arrow arrow-right' onClick={nextSlide}/>
+    </div>
+    
+       <div  className='info'> 
+       <div>
+       <ul>
+          <li>Comfy beds, 8,5 - Based on 425 reviews</li>
+          <li>Room Size: 23m</li>
+          <li>Price: 100$</li>
+          <li>Free toiletries</li>
+        </ul>
+     </div>
+     <div className='buttons'>
+      <Link to={"/information"}>
+      <button>Information</button>
+      </Link>
+      <button>Book</button>
+     </div>
+
+       </div>
+       
+      
+    </div>
+  )
+}
+
+export default RoomInfoCard
