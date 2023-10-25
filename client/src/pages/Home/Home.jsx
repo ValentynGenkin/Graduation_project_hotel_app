e75b8a719c7d54d6e370e18f0d5a3e9272369ec4
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-10-25 14:48:45 +0200

    calendar added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import MainScreenImg from "../../components/MainScreenImg";
 import RandomRoomsCard from "../../components/RandomRoomsCards";
+import RoomSearchBlock from "../../components/RoomSearchBlock";
+
+import "react-calendar/dist/Calendar.css";
 
 const Home = () => {
   return (
     <>
       <MainScreenImg />
+      <RoomSearchBlock />
       <RandomRoomsCard />
     </>
   );
