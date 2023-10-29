faeaa0a68149f9366c81bc88254a24997fd6a979
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-10-29 10:49:59 +0100

    finished client bookings list

--- client/src/pages/Bookings/ClientBookings.jsx
+++ client/src/pages/Bookings/ClientBookings.jsx
@@ -14,7 +14,7 @@ import Room4Image from "../../assets/room4.jpg";
 import "../../Components/CSS/ClientBookings.css";
 
 const responsive = {
-  1024: { items: 1 },
+  1060: { items: 1 },
 };
 
 const ClientBookings = () => {
@@ -37,100 +37,175 @@ const ClientBookings = () => {
   ];
   return (
     <Container className="client-booking-container">
-      <div className="booking-information-block">
-        <div>
-          <span>Check-in: </span>
+      <Container className="client-booking-item">
+        <div className="booking-date-information">
+          <div>
+            <span>Check-in: </span>
 
-          <span>12.11.2023 14:00</span>
-        </div>
-        <div>
-          <span>Check-out: </span>
+            <span>12.11.2023 14:00</span>
+          </div>
+          <div>
+            <span>Check-out: </span>
 
-          <span>15.11.2023 12:00</span>
+            <span>15.11.2023 12:00</span>
+          </div>
         </div>
-      </div>
-      <div className="bookings-description-block">
-        <div className="bookings-img-carousel">
-          <AliceCarousel
-            items={items}
-            responsive={responsive}
-            controlsStrategy="alternate"
-            disableDotsControls
-          />
+        <div className="bookings-description-block">
+          <div className="bookings-img-carousel">
+            <AliceCarousel
+              items={items}
+              responsive={responsive}
+              controlsStrategy="alternate"
+              disableDotsControls
+            />
+          </div>
+          <div className="bookings-description">
+            <h5>Room facilities:</h5>
+            <h6>
+              This air-conditioned double room features a private bathroom, a
+              private entrance and a tea and coffee maker.
+            </h6>
+            <ul>
+              <li>Refrigerator</li>
+              <li>Safety deposit box</li>
+              <li>Hardwood or parquet floors</li>
+              <li>Air conditioning</li>
+              <li>Seating Area</li>
+              <li>
+                <b>Smoking:</b> No smoking
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="bookings-description">
-          <h5>Description</h5>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-          </ul>
-        </div>
-      </div>
-      <div className="booking-information-block">
-        <div>
-          <span>Booking ID: </span>
+        <div className="booking-information-block">
+          <div>
+            <span>Booking ID: </span>
 
-          <span>165168165</span>
-        </div>
-        <div>
-          <span>Amount: </span>
+            <span>165168165</span>
+          </div>
+          <div>
+            <span>Amount: </span>
 
-          <span>270.00 euro</span>
-        </div>
-        <div>
-          <span>Payment method: </span>
+            <span>270.00 euro</span>
+          </div>
+          <div>
+            <span>Payment method: </span>
 
-          <span>iDeal</span>
-        </div>
-        <div>
-          <span>Booking status: </span>
+            <span>iDeal</span>
+          </div>
+          <div>
+            <span>Booking status: </span>
 
-          <span>Confirmed</span>
+            <span>Confirmed</span>
+          </div>
         </div>
-      </div>
-      <Accordion>
-        <Accordion.Item eventKey="0" className="bookings-accordion">
-          <Accordion.Header className="bookings-accordion-header">
-            Send a request
-          </Accordion.Header>
-          <Accordion.Body>
-            <h6>Send a request</h6>
-            <FloatingLabel controlId="floatingTextarea2" label="Comments">
-              <Form.Control
-                as="textarea"
-                placeholder="Leave a comment here"
-                style={{ height: "100px" }}
-              />
-            </FloatingLabel>
-            <Button variant="outline-secondary">Send</Button>
-            <Table className="client-request-table">
-              <thead>
-                <tr>
-                  <th className="booking-table-number">#</th>
-                  <th className="booking-table-request">Request</th>
-                  <th className="booking-table-status">Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Hello I would like ...</td>
-                  <td>Done</td>
-                </tr>
-                <tr>
-                  <td>1</td>
-                  <td>Hello I would like ...</td>
-                  <td>Canceled</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+        <Accordion>
+          <Accordion.Item eventKey="0" className="bookings-accordion">
+            <Accordion.Header className="bookings-accordion-header">
+              Send a request
+            </Accordion.Header>
+            <Accordion.Body>
+              <h6>Send a request</h6>
+              <FloatingLabel controlId="floatingTextarea2" label="Comments">
+                <Form.Control
+                  as="textarea"
+                  placeholder="Leave a comment here"
+                  style={{ height: "100px" }}
+                />
+              </FloatingLabel>
+              <Button variant="outline-secondary" className="request-send-btn">
+                Send
+              </Button>
+              <Table className="client-request-table">
+                <thead>
+                  <tr>
+                    <th className="booking-table-number">#</th>
+                    <th className="booking-table-request">Request</th>
+                    <th className="booking-table-status">Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Hello I would like ...</td>
+                    <td>Done</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Hello I would like ...</td>
+                    <td>Canceled</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Container>
+
+      <h4>Booking history:</h4>
+      <Container className="client-booking-item">
+        <div className="booking-date-information">
+          <div>
+            <span>Check-in: </span>
+
+            <span>12.11.2023 14:00</span>
+          </div>
+          <div>
+            <span>Check-out: </span>
+
+            <span>15.11.2023 12:00</span>
+          </div>
+        </div>
+        <div className="bookings-description-block">
+          <div className="bookings-img-carousel">
+            <AliceCarousel
+              items={items}
+              responsive={responsive}
+              controlsStrategy="alternate"
+              disableDotsControls
+            />
+          </div>
+          <div className="bookings-description">
+            <h5>Room facilities:</h5>
+            <h6>
+              This air-conditioned double room features a private bathroom, a
+              private entrance and a tea and coffee maker.
+            </h6>
+            <ul>
+              <li>Refrigerator</li>
+              <li>Safety deposit box</li>
+              <li>Hardwood or parquet floors</li>
+              <li>Air conditioning</li>
+              <li>Seating Area</li>
+              <li>
+                <b>Smoking:</b> No smoking
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="booking-information-block">
+          <div>
+            <span>Booking ID: </span>
+
+            <span>165168165</span>
+          </div>
+          <div>
+            <span>Amount: </span>
+
+            <span>270.00 euro</span>
+          </div>
+          <div>
+            <span>Payment method: </span>
+
+            <span>iDeal</span>
+          </div>
+          <div>
+            <span>Booking status: </span>
+
+            <span>Confirmed</span>
+          </div>
+        </div>
+      </Container>
     </Container>
   );
 };
