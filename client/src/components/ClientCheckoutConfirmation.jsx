9b598f5fe8977387dd3ef6ba56ee024cbda836eb
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-10-31 13:23:52 +0100

    checkout confirmation page added

--- client/src/components/ClientCheckoutConfirmation.jsx
+++ client/src/components/ClientCheckoutConfirmation.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import "./CSS/ClientCheckoutConfirmation.css";
+import { MdFileDownloadDone } from "react-icons/md";
 
 const ClientCheckoutConfirmation = () => {
   return (
     <Container className="client-checkout-confirmation-container">
-      <h3></h3>
+      <h1 className="confirmation-done">
+        <MdFileDownloadDone />
+      </h1>
+      <br />
+      <h3>Thank You for your payment! </h3>
+      <br />
+      <h5>Total payment amount: 250.00 euro</h5>
+      <br />
+      <h6>Payment ID: 4564614345</h6>
+      <br />
+      <h6>Payment method: iDeal</h6>
+      <br />
+      <p>You will receive an email with your booking information.</p>
     </Container>
   );
 };
