10fd3dbbd797fefdcbe8c1fa7a5c5bbec97090a6
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-11-06 16:42:13 +0100

    remove room from booking button created

---
+++ client/src/components/RemoveRoomFromBookingButton.jsx
@@ -0,0 +1,62 @@
+import React, { useContext } from "react";
+import useFetch from "../hooks/useFetch";
+import { BookingContext } from "../contexts/BookingContext";
+import PropTypes from "prop-types";
+
+const RemoveRoomFromBookingButton = ({
+  roomId,
+  bookingDetailId,
+  className,
+}) => {
+  RemoveRoomFromBookingButton.propTypes = {
+    roomId: PropTypes.string.isRequired,
+    bookingDetailId: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired,
+  };
+  const { handleBookingContext } = useContext(BookingContext);
+
+  const { isLoading, error, performFetch } = useFetch(
+    "/booking/removeRoomFromBooking",
+    (response) => {
+      if (response.success === true) {
+        handleBookingContext();
+      }
+    }
+  );
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    performFetch({
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        roomId: roomId,
+        bookingDetailId: bookingDetailId,
+      }),
+    });
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <p>loading</p>
+      ) : error ? (
+        <>
+          <p>Something went wrong. Error:{error.toString()}</p>
+          <button className={className} onClick={handleClick}>
+            Remove Room Button
+          </button>
+        </>
+      ) : (
+        <button className={className} onClick={handleClick}>
+          Remove Room Button
+        </button>
+      )}
+    </>
+  );
+};
+
+export default RemoveRoomFromBookingButton;
