fea46066ef6868c1cad76a34183d3934c507f30d
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-11-14 13:43:44 +0100

    context added

--- client/src/components/ClientCheckout.jsx
+++ client/src/components/ClientCheckout.jsx
@@ -10,7 +10,7 @@ import {
 import { default as useFetchCheckout } from "../hooks/useFetch";
 import "./CSS/ClientCheckout.css";
 import Input from "./InputComponent";
-import { useBookingContext } from "../contexts/BookingContext";
+import { BookingContext } from "../contexts/BookingContext";
 import { dateFormatter } from "../util/dateFormatter";
 import Carousel from "react-bootstrap/Carousel";
 import { useNavigate } from "react-router-dom";
@@ -20,10 +20,11 @@ import CreditCardImg from "../assets/credit-card.png";
 import { isValidEmail } from "../util/emailValidation";
 import { isValidName } from "../util/nameValidation";
 import { isValidPhoneNumber } from "../util/phoneNumberValidation";
+import { useContext } from "react";
 
 const ClientCheckout = () => {
   const serverDomain = window.location.origin;
-  const { bookingContext, handleBookingContext } = useBookingContext();
+  const { bookingContext, handleBookingContext } = useContext(BookingContext);
   const navigation = useNavigate();
   const [userData, setUserData] = useState({
     firstname: "",
