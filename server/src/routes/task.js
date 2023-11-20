ba75501ed067fdbac593753b333f8312f4e8e0d1
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-11-20 09:47:54 +0100

    update task endpoint created

--- server/src/routes/task.js
+++ server/src/routes/task.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { addTask } from "../controllers/task.js";
+import { addTask, updateTask } from "../controllers/task.js";
 import {
   checkTaskPermission,
+  getAdminAccess,
   getCustomerAccess,
 } from "../middlewares/database/databaseErrorHelpers.js";
 
 const taskRouter = express.Router();
 
 taskRouter.post("/add", [getCustomerAccess, checkTaskPermission], addTask);
+taskRouter.put("/update", getAdminAccess, updateTask);
 
 export default taskRouter;
